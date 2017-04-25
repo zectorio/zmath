9a9bc49f5ffb9103eab07c659c4da12839766d9c
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-04-25 07:04:04 -0400

    Add angle conversion routines

--- src/index.js
+++ src/index.js
@@ -7,11 +7,30 @@ import {Transform, Translation, Rotation} from './transform'
 import geom from './geom'
 import AABB from './aabb'
 
+/**
+ * Convert angle to degrees
+ * @param {number} angleInRadians
+ * @returns {number}
+ */
+function toDeg(angleInRadians) {
+  return 180 * angleInRadians / Math.PI;
+}
+
+/**
+ * Convert angle to radians
+ * @param {number} angleInDegrees
+ * @returns {number}
+ */
+function toRad(angleInDegrees) {
+  return Math.PI * angleInDegrees / 180;
+}
+
 export {
   vec2,
   Transform,
   Translation,
   Rotation,
   AABB,
-  geom
+  geom,
+  toDeg, toRad
 };
